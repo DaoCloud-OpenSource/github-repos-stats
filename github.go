d75fd0b70c2887de545fa4b543eb6ca4066c9510
Paco Xu <paco.xu@daocloud.io>
2023-01-12 17:11:37 +0800

    sync main

--- github.go
+++ github.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"path"
 	"regexp"
@@ -18,6 +19,7 @@ import (
 
 	"github.com/google/go-github/v42/github"
 	"github.com/olekukonko/tablewriter"
+	"golang.org/x/oauth2"
 )
 
 var (
@@ -25,7 +27,7 @@ var (
 	projectsPath   string
 )
 
-var reposTitle = "## The CNCF repos\n"
+var reposTitle = "### The DaoCloud open-source repos\n"
 
 func init() {
 	flag.StringVar(&githubUserName, "username", "", "github user name")
@@ -72,21 +74,47 @@ func fetchAllRepos(projectsPath string, client *github.Client) ([]*github.Reposi
 	log.Println("repos: ", reposList)
 	// #TODO #2 Github has a rate limit of 60 per hour, so don't add more than 60 projects now
 	var skippedRepos []string
+	length := len(reposList)
 	for repo, _ := range reposList {
 		if len(strings.Split(repo, "/")) == 2 {
 			splits := strings.Split(repo, "/")
 			// org/reponame is splits
 			repository, _, err := client.Repositories.Get(context.Background(), splits[0], splits[1])
 			if err != nil {
-				log.Printf("WARNING: skip repo: %s for error: %s", repo, err)
-				skippedRepos = append(skippedRepos, repo)
-				continue
+				// for API rate limit error, sleep for a while
+				if strings.Contains(err.Error(), "403 API rate limit") {
+					if client == nil {
+						log.Printf("WARNING: sleep 3600 seconds for unauthorized github API rate limit 60 per hour: %s ", repo)
+						time.Sleep(3600 * time.Second)
+					} else {
+						// TODO: to get the date after `until` may be better
+						log.Printf("WARNING: sleep 60 seconds for authorized github API rate limit 5000 per hour: %s ", repo)
+						time.Sleep(60 * time.Second)
+					}
+				}
+				// retry once
+				repository, _, err = client.Repositories.Get(context.Background(), splits[0], splits[1])
+				if err != nil {
+					log.Printf("WARNING: skip repo: %s for error: %s", repo, err)
+					skippedRepos = append(skippedRepos, repo)
+					continue
+				}
 			}
 			allRepos = append(allRepos, repository)
 		} else {
 			log.Printf("WARNING: skip repo: %s for error not a github repo", repo)
 		}
 
+		// api limit by default 60 times per hour
+		if client == nil && length > 60 {
+			log.Println("Info: sleep 60 seconds as github API rate limit is 60 per hour for unauthenized user.")
+			time.Sleep(60 * time.Second)
+		}
+		// api limit by default 60 times per hour
+		if client != nil && length > 5000 {
+			log.Println("Info: sleep 1 seconds as github API rate limit is 5000 per hour for authenized user.")
+			time.Sleep(1 * time.Second)
+		}
 	}
 	log.Println("repos: ", reposList)
 	return allRepos, skippedRepos
@@ -148,7 +176,19 @@ func makeReposString(repos []*github.Repository) string {
 
 func main() {
 	flag.Parse()
-	client := github.NewClient(nil)
+	var client *github.Client
+	if token, ok := os.LookupEnv("GITHUB_TOKEN"); ok {
+		httpClient := &http.Client{
+			Transport: &oauth2.Transport{
+				Base:   http.DefaultTransport,
+				Source: oauth2.ReuseTokenSource(nil, oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})),
+			},
+		}
+		client = github.NewClient(httpClient)
+	} else {
+		client = github.NewClient(nil)
+
+	}
 	repos, skippedRepos := fetchAllRepos(projectsPath, client)
 	// change sort logic here
 	sort.Slice(repos[:], func(i, j int) bool {
@@ -158,7 +198,7 @@ func main() {
 	newContentString := makeReposString(repos)
 	log.Println("Repos Status: \n", newContentString)
 	newContentString += "\n\n"
-	newContentString += "## Skipped repos\n"
+	newContentString += "#### Skipped repos\n"
 	newContentString += strings.Join(skippedRepos, "\n")
 
 	readMeFile := path.Join(os.Getenv("GITHUB_WORKSPACE"), "README.md")
